5cce6f52a9aa6b2dfbf237fe78e51a8c20bf5fe1
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-19 23:30:10 +0700

    add form validation on forgot password

--- src/screens/Forgot.js
+++ src/screens/Forgot.js
@@ -1,25 +1,52 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Container, Content, Button, Text, Item, Input} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 export default function Forgot() {
+  const schema = Yup.object().shape({
+    email: Yup.string()
+      .email('Invalid email')
+      .max(50, 'Max 50 characters')
+      .required('Required field'),
+  });
+
   return (
-    <Container>
-      <Content style={styles.padding}>
-        <Text style={styles.header}>Forgot your password?</Text>
-        <Text>
-          To reset your password, please enter your registered email address.
-        </Text>
-        <Item>
-          <Input placeholder="Email address" />
-        </Item>
-      </Content>
-      <View style={styles.padding}>
-        <Button block style={styles.btnColor}>
-          <Text>next</Text>
-        </Button>
-      </View>
-    </Container>
+    <Formik
+      initialValues={{
+        email: '',
+      }}
+      validationSchema={schema}
+      onSubmit={(values) => console.log(values)}>
+      {({handleChange, handleBlur, handleSubmit, values, touched, errors}) => (
+        <Container>
+          <Content style={styles.padding}>
+            <Text style={styles.header}>Forgot your password?</Text>
+            <Text>
+              To reset your password, please enter your registered email
+              address.
+            </Text>
+            <Item>
+              <Input
+                placeholder="Email address"
+                onChangeText={handleChange('email')}
+                onBlur={handleBlur('email')}
+                value={values.email}
+              />
+            </Item>
+            {touched.email && errors.email && (
+              <Text style={styles.error}>{errors.email}</Text>
+            )}
+          </Content>
+          <View style={styles.padding}>
+            <Button block style={styles.btnColor} onPress={handleSubmit}>
+              <Text>next</Text>
+            </Button>
+          </View>
+        </Container>
+      )}
+    </Formik>
   );
 }
 
