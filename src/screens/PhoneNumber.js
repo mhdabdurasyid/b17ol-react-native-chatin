8d87ee582d11fad1a4d0adc0f4b4e45a727c59ae
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-20 00:34:50 +0700

    add form validation on phone checking form

--- src/screens/PhoneNumber.js
+++ src/screens/PhoneNumber.js
@@ -1,28 +1,55 @@
 import React from 'react';
 import {StyleSheet, View} from 'react-native';
 import {Container, Content, Button, Text, Item, Input} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 export default function PhoneNumber() {
+  const schema = Yup.object().shape({
+    phone: Yup.string()
+      .min(10, 'Min 10 characters')
+      .max(12, 'Max 12 characters')
+      .required('Required field'),
+  });
+
   return (
-    <Container>
-      <Content style={styles.padding}>
-        <Text style={styles.header}>
-          What's the phone number for this device?
-        </Text>
-        <Text>
-          By tapping the next button, you accept Chatin's Terms and Conditions
-          of Use and Privacy Policy.
-        </Text>
-        <Item>
-          <Input placeholder="Phone Number" keyboardType={'number-pad'} />
-        </Item>
-      </Content>
-      <View style={styles.padding}>
-        <Button block style={styles.btnColor}>
-          <Text>next</Text>
-        </Button>
-      </View>
-    </Container>
+    <Formik
+      initialValues={{
+        phone: '',
+      }}
+      validationSchema={schema}
+      onSubmit={(values) => console.log(values)}>
+      {({handleChange, handleBlur, handleSubmit, values, touched, errors}) => (
+        <Container>
+          <Content style={styles.padding}>
+            <Text style={styles.header}>
+              What's the phone number for this device?
+            </Text>
+            <Text>
+              By tapping the next button, you accept Chatin's Terms and
+              Conditions of Use and Privacy Policy.
+            </Text>
+            <Item>
+              <Input
+                placeholder="Phone Number"
+                keyboardType={'number-pad'}
+                onChangeText={handleChange('phone')}
+                onBlur={handleBlur('phone')}
+                value={values.phone}
+              />
+            </Item>
+            {touched.phone && errors.phone && (
+              <Text style={styles.error}>{errors.phone}</Text>
+            )}
+          </Content>
+          <View style={styles.padding}>
+            <Button block style={styles.btnColor} onPress={handleSubmit}>
+              <Text>next</Text>
+            </Button>
+          </View>
+        </Container>
+      )}
+    </Formik>
   );
 }
 
@@ -39,5 +66,9 @@ const styles = StyleSheet.create({
   },
   btnColor: {
     backgroundColor: '#6ab6e1',
+  },
+  error: {
+    fontSize: 12,
+    color: 'red',
   },
 });
