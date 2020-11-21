3d14ad5a20fa58698e7d3122c1edd3c534006eb0
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-21 11:21:07 +0700

    add form vaidation on entry password screen

--- src/screens/EnterPassword.js
+++ src/screens/EnterPassword.js
@@ -1,22 +1,69 @@
 import React from 'react';
-import {StyleSheet, View} from 'react-native';
+import {StyleSheet, View, TouchableOpacity} from 'react-native';
 import {Container, Content, Fab, Text, Item, Input, Icon} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 export default function EnterPassword() {
+  const schema = Yup.object().shape({
+    password: Yup.string()
+      .min(6, 'Password required 6 character')
+      .required('Required field'),
+  });
+
   return (
-    <Container>
-      <Content style={styles.padding}>
-        <Text style={styles.header}>Enter your password</Text>
-        <Item>
-          <Input placeholder="Password" />
-        </Item>
-      </Content>
-      <View>
-        <Fab containerStyle={{}} style={styles.fabColor} position="bottomRight">
-          <Icon type="FontAwesome5" name="arrow-right" />
-        </Fab>
-      </View>
-    </Container>
+    <Formik
+      initialValues={{
+        password: '',
+      }}
+      validationSchema={schema}
+      onSubmit={(values) => console.log(values)}>
+      {({
+        handleChange,
+        handleBlur,
+        handleSubmit,
+        values,
+        touched,
+        errors,
+        handleReset,
+      }) => (
+        <Container>
+          <Content style={styles.padding}>
+            <Text style={styles.header}>Enter your password</Text>
+            <Item>
+              <Input
+                placeholder="Password"
+                secureTextEntry
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+              {values.password.length !== 0 && (
+                <TouchableOpacity onPress={handleReset}>
+                  <Icon
+                    type="MaterialIcons"
+                    name="close"
+                    style={styles.iconSize}
+                  />
+                </TouchableOpacity>
+              )}
+            </Item>
+            {touched.password && errors.password && (
+              <Text style={styles.error}>{errors.password}</Text>
+            )}
+          </Content>
+          <View>
+            <Fab
+              containerStyle={{}}
+              style={styles.fabColor}
+              position="bottomRight"
+              onPress={handleSubmit}>
+              <Icon type="FontAwesome5" name="arrow-right" />
+            </Fab>
+          </View>
+        </Container>
+      )}
+    </Formik>
   );
 }
 
