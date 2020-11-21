af34930a898cf153ea89137f12ac4066ce22333b
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-21 11:00:10 +0700

    add form validation on create password

--- src/screens/CreatePassword.js
+++ src/screens/CreatePassword.js
@@ -1,28 +1,71 @@
 import React from 'react';
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import {StyleSheet, View} from 'react-native';
 import {Container, Content, Fab, Text, Item, Input, Icon} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 export default function CreatePassword() {
+  const schema = Yup.object().shape({
+    password: Yup.string()
+      .min(6, 'Password required 6 character')
+      .required('Required field'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('password'), null], 'Password must match')
+      .required('Required field'),
+  });
+
   return (
-    <Container>
-      <Content style={styles.padding}>
-        <Text style={styles.header}>Create password</Text>
-        <Text>
-          Use at least one letter, one number, and four other characters
-        </Text>
-        <Item>
-          <Input placeholder="Password" secureTextEntry />
-        </Item>
-        <Item>
-          <Input placeholder="Reenter password" secureTextEntry />
-        </Item>
-      </Content>
-      <View>
-        <Fab containerStyle={{}} style={styles.fabColor} position="bottomRight">
-          <Icon type="FontAwesome5" name="arrow-right" />
-        </Fab>
-      </View>
-    </Container>
+    <Formik
+      initialValues={{
+        password: '',
+        confirmPassword: '',
+      }}
+      validationSchema={schema}
+      onSubmit={(values) => console.log(values)}>
+      {({handleChange, handleBlur, handleSubmit, values, touched, errors}) => (
+        <Container>
+          <Content style={styles.padding}>
+            <Text style={styles.header}>Create password</Text>
+            <Text>
+              Use at least one letter, one number, and four other characters
+            </Text>
+            <Item>
+              <Input
+                placeholder="Password"
+                secureTextEntry
+                onChangeText={handleChange('password')}
+                onBlur={handleBlur('password')}
+                value={values.password}
+              />
+            </Item>
+            {touched.password && errors.password && (
+              <Text style={styles.error}>{errors.password}</Text>
+            )}
+            <Item>
+              <Input
+                placeholder="Reenter password"
+                secureTextEntry
+                onChangeText={handleChange('confirmPassword')}
+                onBlur={handleBlur('confirmPassword')}
+                value={values.confirmPassword}
+              />
+            </Item>
+            {touched.confirmPassword && errors.confirmPassword && (
+              <Text style={styles.error}>{errors.confirmPassword}</Text>
+            )}
+          </Content>
+          <View>
+            <Fab
+              containerStyle={{}}
+              style={styles.fabColor}
+              position="bottomRight"
+              onPress={handleSubmit}>
+              <Icon type="FontAwesome5" name="arrow-right" />
+            </Fab>
+          </View>
+        </Container>
+      )}
+    </Formik>
   );
 }
 
