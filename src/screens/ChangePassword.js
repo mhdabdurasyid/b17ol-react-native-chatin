0bf6f4fed8fbf11230b313a16d9b8588d4de5ace
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-22 09:57:12 +0700

    add form validation on change password

--- src/screens/ChangePassword.js
+++ src/screens/ChangePassword.js
@@ -1,22 +1,62 @@
 import React from 'react';
 import {StyleSheet} from 'react-native';
 import {Container, Content, Text, Button, Item, Input} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 export default function ChangePassword() {
+  const schema = Yup.object().shape({
+    newPassword: Yup.string()
+      .min(6, 'Password required 6 characters')
+      .max(20, 'Password required max 20 characters')
+      .required('Required field'),
+    confirmPassword: Yup.string()
+      .oneOf([Yup.ref('newPassword'), null], 'New password must match')
+      .required('Required field'),
+  });
+
   return (
-    <Container>
-      <Content style={styles.padding}>
-        <Item>
-          <Input placeholder="Password (6 to 20 chars)" />
-        </Item>
-        <Item>
-          <Input placeholder="Re-confirm password" />
-        </Item>
-      </Content>
-      <Button block style={styles.btnColor}>
-        <Text>next</Text>
-      </Button>
-    </Container>
+    <Formik
+      initialValues={{
+        newPassword: '',
+        confirmPassword: '',
+      }}
+      validationSchema={schema}
+      onSubmit={(values) => console.log(values)}>
+      {({handleChange, handleBlur, handleSubmit, values, touched, errors}) => (
+        <Container>
+          <Content style={styles.padding}>
+            <Item>
+              <Input
+                placeholder="Password (6 to 20 chars)"
+                onChangeText={handleChange('newPassword')}
+                onBlur={handleBlur('newPassword')}
+                value={values.newPassword}
+                secureTextEntry
+              />
+            </Item>
+            {touched.newPassword && errors.newPassword && (
+              <Text style={styles.error}>{errors.newPassword}</Text>
+            )}
+            <Item>
+              <Input
+                placeholder="Re-confirm password"
+                onChangeText={handleChange('confirmPassword')}
+                onBlur={handleBlur('confirmPassword')}
+                value={values.confirmPassword}
+                secureTextEntry
+              />
+            </Item>
+            {touched.confirmPassword && errors.confirmPassword && (
+              <Text style={styles.error}>{errors.confirmPassword}</Text>
+            )}
+          </Content>
+          <Button block style={styles.btnColor} onPress={handleSubmit}>
+            <Text>next</Text>
+          </Button>
+        </Container>
+      )}
+    </Formik>
   );
 }
 
@@ -28,5 +68,9 @@ const styles = StyleSheet.create({
   },
   btnColor: {
     backgroundColor: '#6ab6e1',
+  },
+  error: {
+    fontSize: 12,
+    color: 'red',
   },
 });
