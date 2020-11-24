87ee5085b8ee5764713aee9d8917a999ea85a726
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-24 20:52:29 +0700

    connect to backend on get friend list

--- src/screens/Friends.js
+++ src/screens/Friends.js
@@ -1,56 +1,104 @@
-import React from 'react';
-import {StyleSheet, View, TouchableOpacity} from 'react-native';
+import React, {useEffect} from 'react';
 import {
-  Container,
-  Content,
-  Text,
-  Icon,
-  Thumbnail,
-  Item,
-  Input,
-} from 'native-base';
+  Alert,
+  FlatList,
+  StyleSheet,
+  View,
+  TouchableOpacity,
+} from 'react-native';
+import {Container, Text, Icon, Item, Input} from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
+import {useDispatch, useSelector} from 'react-redux';
 
-import Avatar from '../assets/img/avatar.png';
+// import actions
+import friendAction from '../redux/actions/friend';
+
+import Contact from '../components/Contact';
 
 export default function Friends({navigation}) {
+  const dispatch = useDispatch();
+  const friend = useSelector((state) => state.friend);
+  const auth = useSelector((state) => state.auth);
+
+  const schema = Yup.object().shape({
+    name: Yup.string().max(20),
+  });
+
+  useEffect(() => {
+    dispatch(friendAction.getFriendList('', auth.token));
+    // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [dispatch]);
+
   function addFriend() {
     navigation.navigate('Add_Friend');
   }
 
+  function searchFriend(values) {
+    dispatch(friendAction.getFriendList(values.name, auth.token));
+  }
+
+  useEffect(() => {
+    if (friend.friendIsError) {
+      Alert.alert("User isn't found!");
+      dispatch(friendAction.resetMsg());
+    }
+  });
+
   return (
     <Container>
-      <View>
-        <View style={[styles.padding, styles.header]}>
-          <Text style={styles.headerText}>Friends</Text>
-          <TouchableOpacity onPress={addFriend}>
-            <Icon
-              type="MaterialIcons"
-              name="person-add"
-              style={styles.iconSize}
-            />
+      <Formik
+        initialValues={{
+          name: '',
+        }}
+        validationSchema={schema}
+        onSubmit={(values) => searchFriend(values)}>
+        {({handleChange, handleBlur, handleSubmit, values, handleReset}) => (
+          <View>
+            <View style={[styles.padding, styles.header]}>
+              <Text style={styles.headerText}>Friends</Text>
+              <TouchableOpacity onPress={addFriend}>
+                <Icon
+                  type="MaterialIcons"
+                  name="person-add"
+                  style={styles.iconSize}
+                />
+              </TouchableOpacity>
+            </View>
+            <Item style={styles.padding}>
+              <Icon type="MaterialIcons" name="search" />
+              <Input
+                placeholder="Search by name"
+                onChangeText={handleChange('name')}
+                onBlur={handleBlur('name')}
+                value={values.name}
+                onSubmitEditing={handleSubmit}
+              />
+              {values.name.length !== 0 && (
+                <TouchableOpacity onPress={handleReset}>
+                  <Icon
+                    type="MaterialIcons"
+                    name="close"
+                    style={styles.iconSize}
+                  />
+                </TouchableOpacity>
+              )}
+            </Item>
+          </View>
+        )}
+      </Formik>
+      <Text style={[styles.friendCount, styles.padding]}>
+        Friends {friend.friendData.length}
+      </Text>
+      <FlatList
+        data={friend.friendData}
+        renderItem={({item}) => (
+          <TouchableOpacity style={styles.padding}>
+            <Contact item={item} />
           </TouchableOpacity>
-        </View>
-        <Item style={styles.padding}>
-          <Icon type="MaterialIcons" name="search" />
-          <Input placeholder="Search by name" />
-        </Item>
-      </View>
-      <Content style={styles.padding}>
-        <Text style={styles.friendCount}>Friends 3</Text>
-        <TouchableOpacity style={styles.contact}>
-          <Thumbnail small source={Avatar} />
-          <Text style={styles.contactName}>Jeanne</Text>
-        </TouchableOpacity>
-
-        <TouchableOpacity style={styles.contact}>
-          <Thumbnail small source={Avatar} />
-          <Text style={styles.contactName}>Jeanne</Text>
-        </TouchableOpacity>
-        <TouchableOpacity style={styles.contact}>
-          <Thumbnail small source={Avatar} />
-          <Text style={styles.contactName}>Jeanne</Text>
-        </TouchableOpacity>
-      </Content>
+        )}
+        keyExtractor={(item) => item.friend}
+      />
     </Container>
   );
 }
@@ -68,15 +116,6 @@ const styles = StyleSheet.create({
   headerText: {
     fontSize: 20,
     fontWeight: 'bold',
-  },
-  contact: {
-    flexDirection: 'row',
-    alignItems: 'center',
-    marginVertical: 10,
-  },
-  contactName: {
-    fontWeight: 'bold',
-    marginLeft: 12,
   },
   iconSize: {
     fontSize: 24,
