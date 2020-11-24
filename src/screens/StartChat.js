1113390e55eb3d255cbb4680dd8574d93dc9f3e5
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-24 18:33:25 +0700

    add form validation

--- src/screens/StartChat.js
+++ src/screens/StartChat.js
@@ -9,25 +9,55 @@ import {
   Item,
   Input,
 } from 'native-base';
+import {Formik} from 'formik';
+import * as Yup from 'yup';
 
 import Avatar from '../assets/img/avatar.png';
 
 export default function StartChat({navigation}) {
+  const schema = Yup.object().shape({
+    name: Yup.string().max(20),
+  });
+
   function doChat() {
     navigation.navigate('Chat');
   }
 
   return (
     <Container>
-      <View>
-        <View style={[styles.padding, styles.header]}>
-          <Text style={styles.headerText}>Start Chat</Text>
-        </View>
-        <Item style={styles.padding}>
-          <Icon type="MaterialIcons" name="search" />
-          <Input placeholder="Search by name" />
-        </Item>
-      </View>
+      <Formik
+        initialValues={{
+          name: '',
+        }}
+        validationSchema={schema}
+        onSubmit={(values) => console.log(values)}>
+        {({handleChange, handleBlur, handleSubmit, values, handleReset}) => (
+          <View>
+            <View style={[styles.padding, styles.header]}>
+              <Text style={styles.headerText}>Start Chat</Text>
+            </View>
+            <Item style={styles.padding}>
+              <Icon type="MaterialIcons" name="search" />
+              <Input
+                placeholder="Search by name"
+                onChangeText={handleChange('name')}
+                onBlur={handleBlur('name')}
+                value={values.name}
+                onSubmitEditing={handleSubmit}
+              />
+              {values.name.length !== 0 && (
+                <TouchableOpacity onPress={handleReset}>
+                  <Icon
+                    type="MaterialIcons"
+                    name="close"
+                    style={styles.iconSize}
+                  />
+                </TouchableOpacity>
+              )}
+            </Item>
+          </View>
+        )}
+      </Formik>
       <Content style={styles.padding}>
         <TouchableOpacity onPress={doChat}>
           <View style={styles.contact}>
@@ -59,5 +89,8 @@ const styles = StyleSheet.create({
   messageSender: {
     fontWeight: 'bold',
     marginLeft: 12,
+  },
+  iconSize: {
+    fontSize: 20,
   },
 });
