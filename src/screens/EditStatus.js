efc2146094f140d29dd9325d631384311ade6ee5
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-24 11:38:40 +0700

    connect to backend on edit status message

--- src/screens/EditStatus.js
+++ src/screens/EditStatus.js
@@ -1,9 +1,32 @@
-import React, {useState} from 'react';
+import React, {useEffect, useState} from 'react';
 import {StyleSheet, TouchableOpacity} from 'react-native';
 import {Container, Content, Text, Button, Item, Input, Icon} from 'native-base';
+import {useDispatch, useSelector} from 'react-redux';
 
-export default function Status() {
-  const [status, setStatus] = useState('Im busy, text me later');
+// import actions
+import profileAction from '../redux/actions/profile';
+
+export default function Status({navigation}) {
+  const dispatch = useDispatch();
+  const profile = useSelector((state) => state.profile);
+  const auth = useSelector((state) => state.auth);
+  const [status, setStatus] = useState(
+    profile.profileData.status ? profile.profileData.status : '',
+  );
+
+  function doEditStatus() {
+    const form = new FormData();
+    form.append('status', status);
+    dispatch(profileAction.editProfile(form, auth.token));
+  }
+
+  useEffect(() => {
+    if (profile.isEdit) {
+      dispatch(profileAction.getProfile(auth.token));
+      dispatch(profileAction.resetEdit());
+      navigation.navigate('Profile');
+    }
+  });
 
   return (
     <Container>
@@ -21,7 +44,8 @@ export default function Status() {
       <Button
         block
         style={styles.btnColor}
-        disabled={status.length === 0 ? true : false}>
+        disabled={status.length === 0 || status.length > 500 ? true : false}
+        onPress={doEditStatus}>
         <Text>ok</Text>
       </Button>
     </Container>
