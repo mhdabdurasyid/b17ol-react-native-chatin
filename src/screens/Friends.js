a9c6aad227e8e70c68c929e8e1532a87e73c5b2e
Muhammad Abdurasyid <mhdabdurasyid@users.noreply.github.com>
2020-11-25 21:02:03 +0700

    add modal to see friend detail

--- src/screens/Friends.js
+++ src/screens/Friends.js
@@ -1,25 +1,35 @@
-import React, {useEffect} from 'react';
+import React, {useEffect, useState} from 'react';
 import {
   Alert,
   FlatList,
+  Modal,
   StyleSheet,
   View,
   TouchableOpacity,
 } from 'react-native';
-import {Container, Text, Icon, Item, Input} from 'native-base';
+import {Container, Text, Icon, Item, Input, Thumbnail} from 'native-base';
 import {Formik} from 'formik';
 import * as Yup from 'yup';
 import {useDispatch, useSelector} from 'react-redux';
+import {API_URL} from '@env';
 
 // import actions
 import friendAction from '../redux/actions/friend';
+import messageAction from '../redux/actions/message';
 
+import Avatar from '../assets/img/avatar.png';
 import Contact from '../components/Contact';
 
 export default function Friends({navigation}) {
   const dispatch = useDispatch();
   const friend = useSelector((state) => state.friend);
   const auth = useSelector((state) => state.auth);
+
+  const [modalVisible, setModalVisible] = useState(false);
+  const [image, setImage] = useState('');
+  const [name, setName] = useState('');
+  const [status, setStatus] = useState('');
+  const [friendId, setFriendId] = useState('');
 
   const schema = Yup.object().shape({
     name: Yup.string().max(20),
@@ -44,6 +54,21 @@ export default function Friends({navigation}) {
       dispatch(friendAction.resetMsg());
     }
   });
+
+  function setModalData(img, _name, _status, _friendId) {
+    setModalVisible(true);
+    setImage(img);
+    setName(_name);
+    setStatus(_status);
+    setFriendId(_friendId);
+  }
+
+  function doChat(_friendId, friendName) {
+    setModalVisible(false);
+    dispatch(messageAction.clearDetail());
+    dispatch(messageAction.getMessageDetail(_friendId, auth.token));
+    navigation.navigate('Chat', {friendId: _friendId, friendName});
+  }
 
   return (
     <Container>
@@ -93,12 +118,54 @@ export default function Friends({navigation}) {
       <FlatList
         data={friend.friendData}
         renderItem={({item}) => (
-          <TouchableOpacity style={styles.padding}>
+          <TouchableOpacity
+            style={styles.padding}
+            onPress={() =>
+              setModalData(
+                item.contact.photo,
+                item.contact.name,
+                item.contact.status,
+                item.friend,
+              )
+            }>
             <Contact item={item} />
           </TouchableOpacity>
         )}
         keyExtractor={(item) => item.friend}
       />
+
+      <Modal animationType="fade" transparent={true} visible={modalVisible}>
+        <View style={styles.centeredView}>
+          <View style={styles.modalView}>
+            <Thumbnail
+              large
+              source={image ? {uri: `${API_URL}${image}`} : Avatar}
+            />
+            <Text style={[styles.headerText, styles.name]}>{name}</Text>
+            <Text style={styles.status}>{status}</Text>
+            <View style={styles.modalFooter}>
+              <TouchableOpacity
+                style={styles.modalIcon}
+                onPress={() => doChat(friendId, name)}>
+                <Icon type="MaterialIcons" name="sms" style={styles.iconSize} />
+                <Text style={styles.textStyle}>Chat</Text>
+              </TouchableOpacity>
+              <TouchableOpacity
+                style={styles.modalIcon}
+                onPress={() => {
+                  setModalVisible(false);
+                }}>
+                <Icon
+                  type="MaterialIcons"
+                  name="close"
+                  style={styles.iconSize}
+                />
+                <Text style={styles.textStyle}>Close</Text>
+              </TouchableOpacity>
+            </View>
+          </View>
+        </View>
+      </Modal>
     </Container>
   );
 }
@@ -125,5 +192,45 @@ const styles = StyleSheet.create({
     marginBottom: 6,
     fontSize: 14,
     fontWeight: 'bold',
+  },
+  centeredView: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+  },
+  modalView: {
+    margin: 20,
+    backgroundColor: 'white',
+    borderRadius: 4,
+    paddingTop: 32,
+    paddingBottom: 24,
+    width: '80%',
+    alignItems: 'center',
+    shadowColor: '#000',
+    shadowOffset: {
+      width: 0,
+      height: 1,
+    },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 2,
+  },
+  textStyle: {
+    fontSize: 12,
+  },
+  name: {
+    marginTop: 20,
+  },
+  status: {
+    fontSize: 14,
+    marginBottom: 24,
+  },
+  modalFooter: {
+    flexDirection: 'row',
+    alignItems: 'center',
+  },
+  modalIcon: {
+    paddingHorizontal: 32,
+    alignItems: 'center',
   },
 });
